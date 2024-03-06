072cc29b619b556ad303f4714e0d398064290a76
TheJessNoLimit <160453713+TheJessNoLimit@users.noreply.github.com>
2024-03-06 11:47:01 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+
+function anwser1(num, correct) {
+    const b1 = document.element = document.getElementById("b1");
+    const b2 = document.element = document.getElementById("b2");  
+    const b3 = document.element = document.getElementById("b3");  
+    b1.setAttribute("disabled", true);
+    b2.setAttribute("disabled", true);
+    b3.setAttribute("disabled", true);
+
+    if (correct == true) {
+        var reply = document.createElement("p");
+        reply.innerHTML = "Ответ на вопрос " + num + " <strong>верный!</strong>";
+        reply.style.color = "green";
+        var parent = document.getElementById("replydiv");
+        
+        
+        parent.appendChild(reply);
+
+    }
+    else {
+        const button = document.getElementsByClassName("buttons1"); 
+        var reply = document.createElement("p");
+        reply.innerHTML = "Ответ на вопрос " + num + " <strong>не верный...</strong>";
+        reply.style.color = "red";
+        var parent = document.getElementById("replydiv");
+        
+    
+        parent.appendChild(reply);
+
+    }
+}
+function anwser2(num, correct) {
+    const c1 = document.element = document.getElementById("c1");
+    const c2 = document.element = document.getElementById("c2");  
+    const c3 = document.element = document.getElementById("c3");  
+    c1.setAttribute("disabled", true);
+    c2.setAttribute("disabled", true);
+    c3.setAttribute("disabled", true);
+    if (correct == true) {
+        const button = document.getElementsByClassName("buttons1"); 
+        var reply = document.createElement("p");
+        reply.innerHTML = "Ответ на вопрос " + num + " <strong>верный!</strong>";
+        reply.style.color = "green";
+        var parent = document.getElementById("replydiv");
+        
+        
+        parent.appendChild(reply);
+
+    }
+    else {
+        const button = document.getElementsByClassName("buttons1"); 
+        var reply = document.createElement("p");
+        reply.innerHTML = "Ответ на вопрос " + num + " <strong>не верный...</strong>";
+        reply.style.color = "red";
+        var parent = document.getElementById("replydiv");
+        
+    
+        parent.appendChild(reply);
+
+    }
+}
+function anwser3(num, correct) {
+    const d1 = document.element = document.getElementById("d1");
+    const d2 = document.element = document.getElementById("d2");  
+    const d3 = document.element = document.getElementById("d3");  
+    d1.setAttribute("disabled", true);
+    d2.setAttribute("disabled", true);
+    d3.setAttribute("disabled", true);
+    if (correct == true) {
+        const button = document.getElementsByClassName("buttons1"); 
+        var reply = document.createElement("p");
+        reply.innerHTML = "Ответ на вопрос " + num + " <strong>верный!</strong>";
+        reply.style.color = "green";
+        var parent = document.getElementById("replydiv");
+        
+        
+        parent.appendChild(reply);
+
+    }
+    else {
+        const button = document.getElementsByClassName("buttons1"); 
+        var reply = document.createElement("p");
+        reply.innerHTML = "Ответ на вопрос " + num + " <strong>не верный...</strong>";
+        reply.style.color = "red";
+        var parent = document.getElementById("replydiv");
+        
+    
+        parent.appendChild(reply);
+
+    }
+}
+function anwser4(num, correct) {
+    const g1 = document.element = document.getElementById("g1");
+    const g2 = document.element = document.getElementById("g2");  
+    const g3 = document.element = document.getElementById("g3");  
+    g1.setAttribute("disabled", true);
+    g2.setAttribute("disabled", true);
+    g3.setAttribute("disabled", true);
+    if (correct == true) {
+        const button = document.getElementsByClassName("buttons1"); 
+        var reply = document.createElement("p");
+        reply.innerHTML = "Ответ на вопрос " + num + " <strong>верный!</strong>";
+        reply.style.color = "green";
+        var parent = document.getElementById("replydiv");
+        
+        
+        parent.appendChild(reply);
+
+    }
+    else {
+        const button = document.getElementsByClassName("buttons1"); 
+        var reply = document.createElement("p");
+        reply.innerHTML = "Ответ на вопрос " + num + " <strong>не верный...</strong>";
+        reply.style.color = "red";
+        var parent = document.getElementById("replydiv");
+        
+    
+        parent.appendChild(reply);
+
+    }
+}
+function anwser5(num, correct) {
+    const a1 = document.element = document.getElementById("a1");
+    const a2 = document.element = document.getElementById("a2");  
+    const a3 = document.element = document.getElementById("a3");  
+    a1.setAttribute("disabled", true);
+    a2.setAttribute("disabled", true);
+    a3.setAttribute("disabled", true);
+    if (correct == true) {
+        const button = document.getElementsByClassName("buttons1"); 
+        var reply = document.createElement("p");
+        reply.innerHTML = "Ответ на вопрос " + num + " <strong>верный!</strong>";
+        reply.style.color = "green";
+        var parent = document.getElementById("replydiv");
+        
+        
+        parent.appendChild(reply);
+
+    }
+    else {
+        const button = document.getElementsByClassName("buttons1"); 
+        var reply = document.createElement("p");
+        reply.innerHTML = "Ответ на вопрос " + num + " <strong>не верный...</strong>";
+        reply.style.color = "red";
+        var parent = document.getElementById("replydiv");
+        
+    
+        parent.appendChild(reply);
+
+    }
+}
